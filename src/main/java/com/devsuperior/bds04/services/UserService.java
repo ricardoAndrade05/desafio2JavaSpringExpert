e43735b0b08f5ab1cdb417b9cad0293335545336
ricardo.andrade <ricardo.andrade05@gmail.com>
2026-01-09 14:30:10 -0300

    Implementação UserDetailsService e UserNotfoundException

---
+++ src/main/java/com/devsuperior/bds04/services/UserService.java
@@ -0,0 +1,37 @@
+package com.devsuperior.bds04.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.devsuperior.bds04.entities.Role;
+import com.devsuperior.bds04.entities.User;
+import com.devsuperior.bds04.projections.UserDetailsProjection;
+import com.devsuperior.bds04.repositories.UserRepository;
+
+@Service
+public class UserService implements UserDetailsService {
+
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		List<UserDetailsProjection> result = this.userRepository.searchUserAndRolesByEmail(username);
+		if (result.size() > 0) {
+			throw new UsernameNotFoundException("Usuario não encontrado: " + username);
+		}
+		User user = new User();
+		user.setEmail(username);
+		user.setPassword(result.get(0).getPassword());
+		for (UserDetailsProjection projection : result) {
+			user.addRoles(new Role(projection.getRoleId(), projection.getAuthority()));
+		}
+		return user;
+	}
+
+}
